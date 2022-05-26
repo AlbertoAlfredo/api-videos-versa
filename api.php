42c142780f36f31e6e4419fcc186cc7054a716a1
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2022-05-26 16:44:14 -0300

    tentando ativar mesnagens de erro

--- api.php
+++ api.php
@@ -1,15 +1,11 @@
 # php 7.*
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-echo "This is a warning error";
-?>
+
 <?php
 // include "conexao.php";
 
 echo "teste";
 
-$sql = "'SELECT * FROM videos'";
+$sql = "'SELECT * FROM `videos`'";
 
 $result = $conn->query($sql);
 
