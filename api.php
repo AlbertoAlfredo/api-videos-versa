3301361fb648cf0a545b785b1155a8de6a7eeb5b
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2022-05-26 17:10:54 -0300

    Conexao resolvida, arrumando query

--- api.php
+++ api.php
@@ -4,8 +4,9 @@ ini_set('display_errors', '1');
 echo "This is a warning error";
 ?>
 <?php
-include "conexao.php";
 header('Content-Type: application/json; charset=utf-8');
+include "conexao.php";
+
 $sql = '"SELECT * FROM `videos`"';
 
 $result = $con->query($sql);
