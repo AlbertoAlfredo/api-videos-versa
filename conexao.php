5151754d47c2e937fb004a72004e37dca4918f2f
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2022-05-26 16:34:46 -0300

    tentando ativar mesnagens de erro

--- conexao.php
+++ conexao.php
@@ -7,9 +7,9 @@ $usuario = $USUARIO;
 $senha = $SENHA;
 $banco = $BANCO;
 
-if ($banco != "") {
-	$con = mysqli_connect($servidor,$usuario,$senha, $banco);	
-}
+
+$con = mysqli_connect($servidor,$usuario,$senha, $banco);	
+
 // Check connection
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
