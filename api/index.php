104417a172ca353a1d042e740cc43222b55c5b7e
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2022-05-27 10:25:34 -0300

    Resolvendo erros no tratamento do parametro passado

--- api/index.php
+++ api/index.php
@@ -4,10 +4,10 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ?>
 <?php
-$video = ( isset($_GET['video']) ) ? $_GET['video'] : null;;
-$id = ( isset($_GET['id']) ) ? $_GET['id'] : null;;
-$nome = ( isset($_GET['nome']) ) ? $_GET['nome'] : null;;
-$url = ( isset($_GET['url']) ) ? $_GET['url'] : null;;
+$video = ( isset($_GET['video']) ) ? $_GET['video'] : null;
+$id = ( isset($_GET['id']) ) ? $_GET['id'] : null;
+$nome = ( isset($_GET['nome']) ) ? $_GET['nome'] : null;
+$url = ( isset($_GET['url']) ) ? $_GET['url'] : null;
 $parametros = "video: $video, id: $id, nome:  $nome, url: $url";
 
 if($video == "post") {
