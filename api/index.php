a7a1ffb4c41e6cbd4ee8e452b351b5d929690edf
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2022-05-27 10:09:30 -0300

    Resolvendo erros no tratamento do parametro passado

--- api/index.php
+++ api/index.php
@@ -9,18 +9,23 @@ $video = filter_input(INPUT_GET, 'video');
 $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 $nome = filter_input(INPUT_GET, 'nome');
 $url = filter_input(INPUT_GET, 'url', FILTER_VALIDATE_URL);
+$parametros = "video: $video, id: $id, nome:  $nome, url: $url";
 switch ($video) {
     case "post":
         addVideos($nome, $url);
+        print $parametros;
         break;
     case "put":
         editVideo($id, $nome, $url);
+        print $parametros;
         break;
     case "delete":
         deleteVideo($id);
+        print $parametros;
         break;
     default:
         getVideos();
+        print $parametros;
         break;
 }
 
